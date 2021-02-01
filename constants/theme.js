9d3625be3ccaa1d1320efb90680cabd1d05db146
guillainbisimwa <guillainbisimwa@hotmail.fr>
2021-02-01 19:27:31 +0200

    Add constants file (theme)

---
+++ constants/theme.js
@@ -0,0 +1,60 @@
+const colors = {
+  accent: "#F3534A",
+  primary: "#00ACED",
+  secondary: "#2BDA8E",
+  secondary2: "#144E6F",
+  tertiary: "#FFE358",
+  black: "#323643",
+  white: "#FFFFFF",
+  gray: "#9DA3B4",
+  gray2: "#C5CCD6",
+  gray3: "#F6F6F7",
+  bg_list: "#E5F3FA",
+  error: "#a12",
+  success: "#1a1",
+  profile_bg: '#F1F6F8'
+
+};
+
+const sizes = {
+  // global sizes
+  base: 16,
+  font: 14,
+  radius: 6,
+  padding: 25,
+
+  // font sizes
+  h1: 26,
+  h2: 20,
+  h3: 18,
+  title: 18,
+  header: 16,
+  body: 14,
+  caption: 12
+};
+
+const fonts = {
+  h1: {
+    fontSize: sizes.h1
+  },
+  h2: {
+    fontSize: sizes.h2
+  },
+  h3: {
+    fontSize: sizes.h3
+  },
+  header: {
+    fontSize: sizes.header
+  },
+  title: {
+    fontSize: sizes.title
+  },
+  body: {
+    fontSize: sizes.body
+  },
+  caption: {
+    fontSize: sizes.caption
+  }
+};
+
+export { colors, sizes, fonts };
